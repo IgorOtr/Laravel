21e4219706b5abab5ba2c436da2f99a504ca4f7d
IgorOtr <igorotero.muniz@gmail.com>
2023-10-30 17:18:13 -0300

    MainController created

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\MainController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/home', [MainController::class, 'index'])->name('home.index');
+
+// Route::get('/contact', [MainController::class, 'renderContact']);
