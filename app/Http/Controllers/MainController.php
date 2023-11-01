941bc6db1d5a688d9a00d66b6e98c2a73e09f908
IgorOtr <igorotero.muniz@gmail.com>
2023-11-01 10:58:25 -0300

    Return all data from table

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -3,11 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Models\Post;
 
 class MainController extends Controller
 {
     public function index()
     {
-        return view('home');
+
+        $allPosts = DB::table('postblog')->get();
+
+        return view('home', compact('allPosts'));
     }
 }
